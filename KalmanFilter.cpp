2b7ca1668761b04e2978821dc497e7ae8f532bd3
Felix Geyer <fel.geyer@gmail.com>
2016-08-26 14:38:03 +0200

    Remove incorrect static from function implementation

--- KalmanFilter.cpp
+++ KalmanFilter.cpp
@@ -2,13 +2,13 @@
 
 namespace Filter
 {
-  static void KalmanFilter::sense(GaussianDistribution &belief,
+  void KalmanFilter::sense(GaussianDistribution &belief,
 				  const GaussianDistribution &measurement)
   {
     belief = normalizedProduct(belief, measurement);
   }
 
-  static void KalmanFilter::act(GaussianDistribution &belief,
+  void KalmanFilter::act(GaussianDistribution &belief,
 				const GaussianDistribution movement)
   {
     belief = conv(belief, movement);
